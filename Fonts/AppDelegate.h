9b9b18bd888dd04f96d70e185189f2bede987d05
Alexey B <phlood@yandex.ru>
2020-06-14 17:19:11 +0300

    Initial Commit

---
+++ Fonts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fonts
+//
+//  Created by Alexey Baryshnikov on 14.06.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Fonts/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Fonts
+//
+//  Created by Alexey Baryshnikov on 14.06.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
